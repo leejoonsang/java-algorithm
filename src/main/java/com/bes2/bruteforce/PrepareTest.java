a5458cee86d92781151c2756685cccb2614548ce
Sangjoon <djm04212@naver.com>
2022-10-31 09:45:17 +0900

    [프로그래머스]모의고사(진행중) - 1번 학생 확인

--- src/main/java/com/bes2/bruteforce/PrepareTest.java
+++ src/main/java/com/bes2/bruteforce/PrepareTest.java
@@ -2,12 +2,12 @@ package com.bes2.bruteforce;
 
 public class PrepareTest {
     public int[] solution(int[] answers) {
-        int[] answer = {};
+        int[] answer = {0, 0, 0};
         int[] student1Answer = {1, 2, 3, 4, 5};
 
         for(int i=0; i<answers.length; i++){
             if(student1Answer[i%answers.length] == answers[i]){
-
+                answer[0]++;
             }
         }
 
