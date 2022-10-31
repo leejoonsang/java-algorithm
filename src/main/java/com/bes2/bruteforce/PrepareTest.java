446b430c0fbe747064359ef3476284b7445df5e2
Sangjoon <djm04212@naver.com>
2022-10-31 10:53:02 +0900

    [프로그래머스] 모의고사 - 가장 많은 문제를 맞힌 사람 구하는 중

--- src/main/java/com/bes2/bruteforce/PrepareTest.java
+++ src/main/java/com/bes2/bruteforce/PrepareTest.java
@@ -1,29 +1,48 @@
 package com.bes2.bruteforce;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class PrepareTest {
     public int[] solution(int[] answers) {
-        int[] answer = {0, 0, 0};
+        int[] temp = {0, 0, 0};
+        List<Integer> answer = new ArrayList<>();
         int[] student1 = {1, 2, 3, 4, 5};
         int[] student2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] student3 = {3, 3, 1, 1, 2 ,2, 4, 4, 5, 5};
 
         for(int i=0; i<answers.length; i++){
             if(student1[i%answers.length] == answers[i]){
-                answer[0]++;
+                temp[0]++;
             }
         }
         for(int i=0; i<answers.length; i++){
             if(student2[i%answers.length] == answers[i]){
-                answer[1]++;
+                temp[1]++;
             }
         }
         for(int i=0; i<answers.length; i++){
             if(student3[i%answers.length] == answers[i]){
-                answer[2]++;
+                temp[2]++;
             }
         }
+
+        int maxScore = 0;
+        for(int j=0; j<temp.length; j++){
+            if(temp[j] > maxScore){
+                maxScore = temp[j];
+            }
+        }
+
+        for(int j=0; j<temp.length; j++){
+            if(temp[j] == maxScore){
+                answer.add(temp[j]);
+            }
+        }
+
+        answer.toArray(new String[answer.size()]);
 
         return answer;
     }
