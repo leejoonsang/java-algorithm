05562df332042424cea5fb4443fd2c2dcd487201
Sangjoon <djm04212@naver.com>
2022-11-01 09:50:49 +0900

    소수 찾기 - 1,2단계 (완료)

--- src/main/java/com/bes2/primenumber/PrimeNum.java
+++ src/main/java/com/bes2/primenumber/PrimeNum.java
@@ -1,10 +1,20 @@
 package com.bes2.primenumber;
 
 public class PrimeNum {
-    public boolean isPrimeNum(int n) {
+    // n-1 까지 나눠보는 방법
+    public boolean isPrimeNum1(int num) {
+        for(int i=2; i<=num-1; i++) {
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 
-        for(int i=2; i<=n-1; i++) {
-            if(n % i == 0){
+    public boolean isPrimeNum2(int num) {
+        int halfNum = num/2;
+        for(int i=2; i<=halfNum; i++){
+            if(num % i == 0){
                 return false;
             }
         }
@@ -14,9 +24,13 @@ public class PrimeNum {
     public static void main(String[] args) {
         PrimeNum pn = new PrimeNum();
 
-        System.out.println(pn.isPrimeNum(13));
-        System.out.println(pn.isPrimeNum(17));
-        System.out.println(pn.isPrimeNum(19));
-        System.out.println(pn.isPrimeNum(23));
+        System.out.println(pn.isPrimeNum1(13));
+        System.out.println(pn.isPrimeNum1(20));
+
+        System.out.println(pn.isPrimeNum2(19));
+        System.out.println(pn.isPrimeNum2(24));
+
+        System.out.println(pn.isPrimeNum2(25));
+        System.out.println(pn.isPrimeNum2(36));
     }
 }
