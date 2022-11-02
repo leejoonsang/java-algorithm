b8d985ffb4a3a55c6ecbf3dcf589d685e7678188
Sangjoon <djm04212@naver.com>
2022-11-02 10:46:00 +0900

    [프로그래머스] 소수 찾기(에라토스테네스의 체) 속도 개선 - 2의 배수 제거

--- src/main/java/com/bes2/primenumber/PrimeNumSieve.java
+++ src/main/java/com/bes2/primenumber/PrimeNumSieve.java
@@ -1,11 +1,12 @@
 package com.bes2.primenumber;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PrimeNumSieve {
 
-    public int solution(int n) {
+    public int solutionTemp(int n) {
         List<Integer> list = new ArrayList<>();
         int answer = 0;
 
@@ -25,9 +26,29 @@ public class PrimeNumSieve {
         return answer;
     }
 
+    public boolean[] solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n+1];   // 인덱싱 어떻게?
+        boolean[] check = new boolean[n+1];
+
+        for(int i=2; i<=n; i++) {
+            arr[i] = i;
+            check[i] = true;
+        }
+
+        for(int i=2; i<=n; i++){
+            if(arr[i] % 2 == 0 && arr[i] > 2) {
+                check[i] = false;
+            }
+        }
+
+        // arr[0] ~ arr[9]
+        return check;
+    }
+
     public static void main(String[] args) {
         PrimeNumSieve pns = new PrimeNumSieve();
-        System.out.println(pns.solution(400000));
+        System.out.println(Arrays.toString(pns.solution(10)));
     }
 
 }
