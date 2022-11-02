86c71d17eb9f5c9fe4be277fc820af42620670cc
Sangjoon <djm04212@naver.com>
2022-11-02 10:19:07 +0900

    [프로그래머스] 소수 찾기(에라토스테네스의 체) - 통과 실패

--- src/main/java/com/bes2/primenumber/PrimeNumSieve.java
+++ src/main/java/com/bes2/primenumber/PrimeNumSieve.java
@@ -9,23 +9,25 @@ public class PrimeNumSieve {
         List<Integer> list = new ArrayList<>();
         int answer = 0;
 
-        for(int i=2; i<=50; i++) {
+        for(int i=2; i<=n; i++) {
             list.add(i);
         }
-        answer = list.size();
 
-        for(int i=0; i<list.size(); i++) {
-            if(list.get(i) % 2 == 0 && list.get(i) > 2) {
-                list.remove(i);
+        for(int j=2; j*j<=n; j++){
+            for(int i=0; i<list.size(); i++) {
+                if(list.get(i) % j == 0 && list.get(i) > j) {
+                    list.remove(i);
+                }
             }
         }
         answer = list.size();
+
         return answer;
     }
 
     public static void main(String[] args) {
         PrimeNumSieve pns = new PrimeNumSieve();
-        System.out.println(pns.solution(50));
+        System.out.println(pns.solution(400000));
     }
 
 }
