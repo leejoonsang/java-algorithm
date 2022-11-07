4b275313e14f7b094d7a1de3a958ee534ef8438a
Sangjoon <djm04212@naver.com>
2022-11-07 09:42:59 +0900

    [프로그래머스] 하샤드 수 (완료)

--- src/main/java/com/bes2/harshadnumber/HarshadNum.java
+++ src/main/java/com/bes2/harshadnumber/HarshadNum.java
@@ -2,19 +2,19 @@ package com.bes2.harshadnumber;
 
 public class HarshadNum {
 
-    public boolean solution(int x) throws Exception {
-        boolean answer = true;
+    public boolean solution(int x) {
         int sum = 0;
+        int originalX = x;
 
         while(x > 0){
             sum += x%10;
             x /= 10;
         }
 
-        return answer;
+        return (originalX % sum) == 0;
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         HarshadNum harshadNum = new HarshadNum();
         System.out.println(harshadNum.solution(687));
     }
