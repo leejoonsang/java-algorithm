e8662d46fbaab9208009de4bd8847d949f877554
Sangjoon <djm04212@naver.com>
2022-11-08 11:06:07 +0900

    이진탐색 로직 만들기

--- src/main/java/com/bes2/search/BinarySearch.java
+++ src/main/java/com/bes2/search/BinarySearch.java
@@ -4,19 +4,23 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
         int lowIdx = 0;
-        int highIdx = 10;
-        int midIdx = (lowIdx+highIdx)/2;
-        int target = 7;
+        int highIdx = nums.length-1;
+        int target = 7; int targetIdx = -1;
 
-        if(target > nums[midIdx]){
-            lowIdx = midIdx;
-            midIdx = (lowIdx+highIdx)/2;
-        } else if (target < nums[midIdx]) {
-            highIdx = midIdx;
-            midIdx = (lowIdx+highIdx)/2;
-        }else {
-            System.out.println("``");
+        while(lowIdx <= highIdx) {
+            int midIdx = (lowIdx + highIdx) / 2;
+            int midVal = nums[midIdx];
+            if(midVal > target){
+                highIdx = midIdx - 1;
+            } else if (midVal < target) {
+                lowIdx = midIdx + 1;
+            }else {
+                System.out.println(midIdx);
+                targetIdx = midIdx;
+                break;
+            }
         }
+
 
     }
 }
