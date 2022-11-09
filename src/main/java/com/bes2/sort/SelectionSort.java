91221c15a5043839851801d44d47e5ff0f909608
Sangjoon <djm04212@naver.com>
2022-11-09 10:14:34 +0900

    선택정렬 알고리즘 (+ 내림차순 적용까지)

--- src/main/java/com/bes2/sort/SelectionSort.java
+++ src/main/java/com/bes2/sort/SelectionSort.java
@@ -24,9 +24,31 @@ public class SelectionSort {
         return arr;
     }
 
+    public int[] reverseSelectionSort(int arr[]) {
+        int maxIdx = 0;
+        int temp;
+
+        for(int i=0; i<arr.length-1; i++) {
+            maxIdx = i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[j] > arr[maxIdx]){
+                    maxIdx = j;
+                }
+            }
+
+            temp = arr[maxIdx];
+            arr[maxIdx] = arr[i];
+            arr[i] = temp;
+
+        }
+        return arr;
+
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
         SelectionSort selectionSort = new SelectionSort();
         System.out.println(Arrays.toString(selectionSort.doSelectionSort(arr)));
+        System.out.println(Arrays.toString(selectionSort.reverseSelectionSort(arr)));
     }
 }
