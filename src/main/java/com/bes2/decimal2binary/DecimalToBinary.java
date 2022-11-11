ae3e7d5450178eb7056e65539acb07da478ae389
Sangjoon <djm04212@naver.com>
2022-11-11 11:01:34 +0900

    [프로그래머스] 비밀지도 (완료)

--- src/main/java/com/bes2/decimal2binary/DecimalToBinary.java
+++ src/main/java/com/bes2/decimal2binary/DecimalToBinary.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 public class DecimalToBinary {
 
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        String[] answer = new String[n];
         for(int i=0; i<n; i++) {
-
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i])
+                    .replace("1", "#").replace("0", " ");
+            answer[i] = " ".repeat(n - answer[i].length()) + answer[i];
         }
 
         return answer;
@@ -20,6 +22,6 @@ public class DecimalToBinary {
         DecimalToBinary dTOb = new DecimalToBinary();
         int[] arr1 = new int[]{9, 20, 28, 18, 11};
         int[] arr2 = new int[]{30, 1, 21, 17, 28};
-        System.out.println(Arrays.toString(dTOb.solution(5, arr1, arr2)));
+        System.out.println(Arrays.toString(dTOb.solution(n, arr1, arr2)));
     }
 }
