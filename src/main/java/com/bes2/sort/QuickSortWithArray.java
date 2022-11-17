dcdf0484f254560be193cd85ef0dea98b2275422
Sangjoon <djm04212@naver.com>
2022-11-17 10:27:37 +0900

    퀵 정렬 (완료)

--- src/main/java/com/bes2/sort/QuickSortWithArray.java
+++ src/main/java/com/bes2/sort/QuickSortWithArray.java
@@ -3,24 +3,38 @@ package com.bes2.sort;
 import java.util.Arrays;
 
 public class QuickSortWithArray {
-    public static void main(String[] args) {
-        int[] arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
-        int pivot = arr[arr.length / 2];
+
+    public int[] sort(int[] arr, int startIdx, int endIdx) {
         int leftIdx = 0;
         int rightIdx = arr.length - 1;
+        int pivot = arr[(startIdx+endIdx) / 2];
 
-        while(arr[leftIdx] < pivot) {
-            leftIdx++;
+        while (leftIdx <= rightIdx) {
+            while(arr[leftIdx] < pivot) {
+                leftIdx++;
+            }
+            while(arr[rightIdx] > pivot) {
+                rightIdx--;
+            }
+            if(leftIdx <= rightIdx){
+                int temp = arr[leftIdx];
+                arr[leftIdx] = arr[rightIdx];
+                arr[rightIdx] = temp;
+                leftIdx++;
+                rightIdx--;
+            }
         }
-        while(arr[rightIdx] > pivot) {
-            rightIdx--;
-        }
-        if(arr[leftIdx] > arr[rightIdx]) {
-            int temp = arr[leftIdx];
-            arr[leftIdx] = arr[rightIdx];
-            arr[rightIdx] = temp;
-        }
+        if(startIdx < rightIdx) sort(arr, startIdx, rightIdx);
+        if(leftIdx < endIdx) sort(arr, leftIdx, endIdx);
 
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
+        System.out.println(Arrays.toString(arr));
+
+        QuickSortWithArray qs = new QuickSortWithArray();
+        qs.sort(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
 }
