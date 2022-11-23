bbf2cc9563dba8ec64637461873104088913c05a
Sangjoon <djm04212@naver.com>
2022-11-23 10:49:02 +0900

    [수정] 재귀 사용하지 않고 피보나치 수열 구하기

--- src/main/java/com/bes2/recursive/FibonacciWithoutRecursive.java
+++ src/main/java/com/bes2/recursive/FibonacciWithoutRecursive.java
@@ -2,14 +2,18 @@ package com.bes2.recursive;
 
 import java.util.Arrays;
 
-public class FactorialWithoutRecursive {
-    public static void main(String[] args) {
+public class FibonacciWithoutRecursive {
+    public static long[] solution() {
         long[] arr = new long[50];
         arr[0] = 1;
         arr[1] = 1;
         for(int i=2; i<arr.length; i++) {
             arr[i] = arr[i-2] + arr[i-1];
         }
-        System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution()));
     }
 }
