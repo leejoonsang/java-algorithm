9a11809abde98fa1579988669173d44ac553799e
Sangjoon <djm04212@naver.com>
2022-11-23 09:46:19 +0900

    팩토리얼 계산

---
+++ src/main/java/com/bes2/recursive/Factorial.java
@@ -0,0 +1,18 @@
+package com.bes2.recursive;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static int factorial(int n) {
+        if(n <= 1) {
+            return n;
+        }
+        return n * factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+    }
+}
