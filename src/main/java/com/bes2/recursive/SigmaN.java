65bc2c46cd5d6c2603bb3c5307343afad0cf5c85
Sangjoon <djm04212@naver.com>
2022-11-23 09:44:15 +0900

    재귀 이용해서 1부터 n까지 합 구하기

---
+++ src/main/java/com/bes2/recursive/SigmaN.java
@@ -0,0 +1,18 @@
+package com.bes2.recursive;
+
+import java.util.Scanner;
+
+public class SigmaN {
+    public static int sigma(int n) {
+        if(n == 0) {
+            return n;
+        }
+        return n + sigma(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sigma(n));
+    }
+}
